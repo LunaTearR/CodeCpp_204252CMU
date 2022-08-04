1b699aad937fd65a9abb74721e9b5752145e7c89
LunaTearR <saengtawanpoopoom@gmail.com>
2022-08-04 15:36:07 +0700

    Edit isBalanced HW05

--- HW05/main.cpp
+++ HW05/main.cpp
@@ -4,6 +4,72 @@
 #include <string>
 using namespace std;
 
+bool isBalanced(string s){
+  stack <char> st;
+  int length = s.length();
+  char ch;
+
+  for(int i = 0; i < length; i++){
+    ch = s[i];
+    if(ch == '('  || ch == '[' || ch == '{'){
+      st.push(ch);
+    }
+    else if(ch == ')'){
+      if(st.empty()){
+        st.push(ch);
+      }
+      else if (st.top() == '('){
+        st.pop();
+      }
+      else {
+        st.push(ch);
+      }
+    }
+    else if(ch == '}'){
+      if(st.empty()){
+        st.push(ch);
+      }
+      else if(st.top() == '{'){
+        st.pop();
+      }
+      else{
+        st.push(ch);
+      }
+    }
+    else if(ch == ']'){
+      if(st.empty()){
+        st.push(ch);
+      }
+      else if(st.top() == '['){
+        st.pop();
+      }
+      else {
+        st.push(ch);
+      }
+    }
+  }
+  if(st.empty()) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+  while (s != "-1") {
+    if(isBalanced(s)) {
+      cout << "Parentheses are balanced" << endl;
+    } else {
+      cout << "Parentheses are not balanced" << endl;
+    }
+    cin >> s;
+  }
+}
+
+
 // bool isBalanced(string s) {
 //   int c1 = 0;
 //   int c2 = 0;
@@ -36,60 +102,3 @@ using namespace std;
 //   }
 // }
 
-
-bool isBalanced(string s) {
-  stack <char> st;
-  int length = s.length();
-  char chartop = st.top();
-  char ch;
-
-  for(int i = 0; i < length; i++){
-    ch = s[i];
-    if(ch == '('  || ch == '[' || ch == '{'){
-      st.push(ch);
-    }
-    else if(ch == ')'){
-      if (chartop == '('){
-        st.pop();
-      }
-      else {
-        st.push(ch);
-      }
-    }
-    else if(ch == '}'){
-      if(chartop == '{'){
-        st.pop();
-      }
-      else{
-        st.push(ch);
-      }
-    }
-    else if(ch == ']'){
-      if(chartop == '['){
-        st.pop();
-      }
-      else {
-        st.push(ch);
-      }
-    }
-  }
-  if(st.empty()) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
-int main() {
-  string s;
-  cin >> s;
-  while (s != "-1") {
-    if(isBalanced(s)) {
-      cout << "Parentheses are balanced" << endl;
-    } else {
-      cout << "Parentheses are not balanced" << endl;
-    }
-    cin >> s;
-  }
-}
