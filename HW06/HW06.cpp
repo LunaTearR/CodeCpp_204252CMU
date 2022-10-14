bd6464b03c5500f652482ba650aea0f3aa92b521
LunaTearR <saengtawanpoopoom@gmail.com>
2022-10-15 04:54:18 +0700

    commit

--- HW06/HW06.cpp
+++ HW06/HW06.cpp
@@ -1,40 +1,143 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-class Team{
+class Team {
     public:
-    int ID;
-    int GD;//Goal Difference = Goal score - Goal conced
-    int PTS;//Point (win = 3, draw = 1, lose = 0)
+        int ID;
+        int GD;  // Goal Difference = Goal score - Goal conced
+        int PTS; // Point (win = 3, draw = 1, lose = 0)
 
-    //Constructor
-    Team(int x, int y, int z) : ID(x), GD(y), PTS(z) {}
-	
-    /*
-         WRITE YOUR CODE HERE
-    */
+        // Constructor
+        Team(int x, int y, int z) : ID(x), GD(y), PTS(z) {}
+
 };
 
 class Scoreboard{
-public:
-    vector<Team> T;
-    int numberOfTeams = 0;
-    
-    Scoreboard(int n){
-        numberOfTeams = n;
-        for(int i=0;i<n;i++){
-            Team x(i,0,0);
-            T.push_back(x);
-        }
-    }
+    public:
+        vector<Team> T;
+        int numberOfTeams = 0;
 
-    void match(int ID1, int ID2,int G1,int G2){
-        /*
-         WRITE YOUR CODE HERE
-         */
-    } 
-    void showTeamAtRank(int i){
-        /*
-         WRITE YOUR CODE HERE
-         */
-    }
+        Scoreboard(int n){
+            numberOfTeams = n;
+            for (int i = 0; i < n; i++){
+                Team x(i, 0, 0);
+                T.push_back(x);
+            }
+        }
+
+        void match(int ID1, int ID2, int G1, int G2){
+            //set position ID
+            int k,j;
+            for(int i = 0;i < T.size();i++){
+                if(T[i].ID == ID1){
+                    k = i;
+                }
+                if(T[i].ID == ID2){
+                    j = i;
+                }
+            }
+            // cal score
+            if(G1 > G2){
+                T[k].PTS += 3;
+                T[k].GD += G1 - G2;
+                T[j].GD += G2 - G1;
+            }
+            else if(G1 < G2){
+                T[j].PTS += 3;
+                T[j].GD += G2 - G1;
+                T[k].GD += G1 - G2;
+            }
+            else{
+                T[j].PTS += 1;
+                T[k].PTS += 1;
+            }
+            //sort Table
+            update_table();
+            
+        }
+        void showTeamAtRank(int i){
+            i--;
+            //Show
+            cout << T[i].ID << " " << T[i].PTS << " " << T[i].GD << endl;
+        }
+        // void sort_score(){
+        //     int i,j,index_id,index_gd,index_pts;
+        //     for(i = 1; i < T.size();i++){
+        //         index_pts = T[i].PTS;
+        //         index_id = T[i].ID;
+        //         index_gd = T[i].GD;
+        //         j = i;
+        //         while((j > 0) && (T[j-1].PTS <= index_pts)){
+        //             if(T[j-1].PTS == T[j].PTS){
+        //                 if(T[j-1].GD < T[j].GD){
+        //                     T[j] = T[j-1];
+        //                     j = j-1;
+        //                 }
+        //                 //if gd and pts equel then pass
+        //                 else if(T[j-1].GD == T[j].GD){
+        //                     j = j-1;
+        //                 }
+        //                 else{
+        //                     break;
+        //                 }
+        //             }
+        //             else{
+        //                 T[j] = T[j-1];
+        //                 j = j-1;
+        //             }
+        //         }
+        //         T[j].PTS = index_pts;
+        //         T[j].ID = index_id;
+        //         T[j].GD = index_gd;
+        //     }
+
+        // }
+        void update_table(){
+            //bubble sort
+            int i,j,temp_pts,temp_gd,temp_id;
+            for(i = (T.size() - 1);i >= 0;i--){
+                for(j = 1;j <= i;j++){
+                    //check score
+                    if(T[j-1].PTS == T[j].PTS){
+                        //check different goal
+                        if(T[j-1].GD < T[j].GD){
+                            temp_pts = T[j-1].PTS;
+                            temp_gd = T[j-1].GD;
+                            temp_id = T[j-1].ID;
+                            T[j - 1] = T[j];
+                            T[j].PTS = temp_pts;
+                            T[j].ID = temp_id;
+                            T[j].GD = temp_gd;
+                        }
+                        else if(T[j-1].GD == T[j].GD){
+                            //check id
+                            if(T[j-1].ID > T[j].ID){
+                                temp_pts = T[j-1].PTS;
+                                temp_gd = T[j-1].GD;
+                                temp_id = T[j-1].ID;
+                                T[j - 1] = T[j];
+                                T[j].PTS = temp_pts;
+                                T[j].ID = temp_id;
+                                T[j].GD = temp_gd;
+                            }
+                            else{
+                                continue;
+                            }
+                        }
+                        else{
+                            continue;
+                        }
+                    }
+                    else if(T[j-1].PTS < T[j].PTS){
+                        temp_pts = T[j-1].PTS;
+                        temp_gd = T[j-1].GD;
+                        temp_id = T[j-1].ID;
+                        T[j - 1] = T[j];
+                        T[j].PTS = temp_pts;
+                        T[j].ID = temp_id;
+                        T[j].GD = temp_gd;
+                    }
+                }
+            }
+        }
 };
--- HW06/main.cpp
+++ HW06/main.cpp
@@ -3,16 +3,42 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
+    int n =4;
+    // cin>>n;
     Scoreboard S(n);
-    S.showTeamAtRank(1) ;
+    S.showTeamAtRank(1);
+    S.showTeamAtRank(2);
+    S.showTeamAtRank(3);
+    S.showTeamAtRank(4);
+
+    cout << "----------------------------" << endl;
     S.match(0,1,2,3);
     S.showTeamAtRank(1);
+    S.showTeamAtRank(2);
+    S.showTeamAtRank(3);
     S.showTeamAtRank(4);
+
+    cout << "----------------------------" << endl;
     S.match(3,2,1,1);
     S.showTeamAtRank(1);
     S.showTeamAtRank(2);
+    S.showTeamAtRank(3);
+    S.showTeamAtRank(4);
+
+    cout << "----------------------------" << endl;
+    S.match(0,2,3,1);
+    S.showTeamAtRank(1);
+    S.showTeamAtRank(2);
+    S.showTeamAtRank(3);
+    S.showTeamAtRank(4);
+
+
+    
+    
+    
+
+
+
     
     return 0;
 }
