b8cc88b1ea2541bb39e7842898a3e67605ac0cbe
LunaTearR <saengtawanpoopoom@gmail.com>
2022-10-15 17:35:25 +0700

    commit

--- HW08/HW08.cpp
+++ HW08/HW08.cpp
@@ -1,32 +1,107 @@
 #include "BSTNode.cpp"
+#include<iostream>
 
+using namespace std;
 class BST {
 
 public:
   BSTNode* root;
-
+  
   BST() {
     root = nullptr;
+    
   }
 
   void insert(int value) {
-    /*
-     Write your code here
-     */
+    BSTNode *new_node = new BSTNode();
+    BSTNode *p,*prev;
+    new_node -> left = NULL;
+    new_node -> right = NULL;
+    new_node -> value = value;
+    if(root == NULL){
+      root = new_node;
+      root -> left = NULL;
+      root -> right = NULL;
+    }
+    else {
+      p = root;
+      while(p != NULL){
+        prev = p;
+        if(p -> value > new_node -> value){
+          p = p -> left;
+        }
+        else {
+          p = p -> right;
+        }
+      }
+      if(prev -> value > new_node -> value){
+        prev -> left = new_node;
+      }
+      else {
+        prev -> right = new_node;
+      }
+    }
   }
 
 
   void remove(int value) {
-    /*
-     Write your code here
-     */
+    
   }
 
   int get_depth(int value) {
-    /*
-     Write your code here
-     */
-    return -1;
+    BSTNode *p,*prev;
+    int depth = 0;
+    if (value == root -> value){
+      return depth;
+    }
+    if(!checkValue(root,value)){
+      return -1;
+    }
+    p = root;
+    while (p -> value != value){
+      if(p -> value > value){
+        p = p -> left;
+        depth++;
+      }
+      else {
+        p = p -> right;
+        depth++;
+      }
+    }
+    return depth;
+  }
+
+  bool checkValue(BSTNode* node, int key){
+    if (node == NULL)
+        return false;
+ 
+    if (node -> value == key)
+        return true;
+ 
+    /* check on left subtree */
+    bool res1 = checkValue(node -> left, key);
+    // node found, no need to look further
+    if(res1) return true;
+ 
+    /* node is not found in left,
+    so check on right subtree */
+    bool res2 = checkValue(node -> right, key);
+
+    return res2;
+  }
+
+  void show(){
+    BSTNode *p;
+    p = root;
+    while (p != NULL){
+      cout << p -> value << endl;
+      p = p -> left;
+    }
+    p = root;
+    while (p != NULL){
+      cout << p -> value << endl;
+      p = p -> right;
+    }
   }
 
 };
--- HW08/main.cpp
+++ HW08/main.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 int main() {
     BST tree;
-
     tree.insert(2);
     tree.insert(1);
-    tree.insert(3);
-    tree.remove(2);
+    tree.insert(4);
+    tree.insert(9);
+    tree.insert(8);
+    tree.insert(7);
+    // tree.remove(2);
     cout << tree.get_depth(1) << endl;
     cout << tree.get_depth(2) << endl;
-    cout << tree.get_depth(3) << endl;
+    cout << tree.get_depth(5) << endl;
 
     return 0;
 }
