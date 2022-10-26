a4645cdcac4daed0bd60037f7dbaf732475a11d9
LunaTearR <saengtawanpoopoom@gmail.com>
2022-10-26 19:42:37 +0700

    commit

--- HW10/HW10.cpp
+++ HW10/HW10.cpp
@@ -1,40 +1,82 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-    /*
-         WRITE YOUR CODE HERE
-    */
+#include <algorithm>
+
 using namespace std;
 class Village{
     int numHouse;
-    /*
-         WRITE YOUR CODE HERE
-    */
+    vector<int> *adjLists;
+    bool *bfs_visited;
+    bool *dfs_visited;
     
 public:
     Village(int V){
-    /*
-         WRITE YOUR CODE HERE
-    */
+     numHouse = V;
+     adjLists = new vector <int> [V];
     }
+
     void addRoad(int src, int dest){
-    /*
-         WRITE YOUR CODE HERE
-    */
+     adjLists[src].push_back(dest);
+     adjLists[dest].push_back(src);
     }
+
+    void sort_adjLists(){
+          for(int i = 0; i < numHouse; i++){
+               sort(adjLists[i].begin(),adjLists[i].end());
+          }
+     }
+
     void BFS(int firstHouse){
-    /*
-         WRITE YOUR CODE HERE
-    */
+     bfs_visited = new bool[numHouse];
+     for(int i = 0; i < numHouse; i++){
+          bfs_visited[i] = false;
+     }
+     queue<int> Q;
+     Q.push(firstHouse);
+     while(!Q.empty()){
+          int u = Q.front();
+          Q.pop();
+          if(bfs_visited[u]){
+               continue;
+          }
+          bfs_visited[u] = true;
+          cout << u << " ";
+          sort_adjLists();
+          vector<int>::iterator it;
+          for(it = adjLists[u].begin(); it != adjLists[u].end();it++){
+               if(!bfs_visited[*it]){
+                    Q.push(*it);
+               }
+          } 
+     }
     }
+
     void DFS(int firstHouse){
-    /*
-         WRITE YOUR CODE HERE
-    */
+     dfs_visited = new bool[numHouse];
+     for(int i = 0; i < numHouse; i++){
+          dfs_visited[i] = false;
+     }
+     DFSVISIT(firstHouse);
+
+     for(int i = 0; i < numHouse; i++){
+          if(dfs_visited[i] == false){
+               DFSVISIT(i);
+          }
+     }
     }
-    /*
-         WRITE YOUR CODE HERE
-    */
+    
+    void DFSVISIT(int vertex){
+     dfs_visited[vertex] = true;
+     cout << vertex << " ";
+     sort_adjLists();
+     vector<int>::iterator it;
+     for(it = adjLists[vertex].begin();it != adjLists[vertex].end(); it++){
+          if(!dfs_visited[*it]){
+               DFSVISIT(*it);
+          }
+     }
+    }
     
 };
 
